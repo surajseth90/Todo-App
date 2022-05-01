a2f9d37ff8b6814aacae88939f4df9f770a260f3
Suraj gupta <surajseth90@gmail.com>
2022-05-01 17:38:59 +0530

    todo list added

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 var example = "working";
+var items = [];
 
 app.get("/", function (req, res) {
-  res.render("list", { exej: example }); //render is used rather than send while using ejs
+  res.render("list", { ejes: items }); //render is used rather than send while using ejs
 });
 
 app.post("/", function (req, res) {
-  console.log("name", req.body.ele1);
+  var item = req.body.ele1;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(8000, function () {
