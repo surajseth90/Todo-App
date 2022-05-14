3b12f48c3a29aef79b9409022fb5841b6daf6084
Suraj gupta <surajseth90@gmail.com>
2022-05-14 13:29:14 +0530

    get api added

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ const bodyParser = require("body-parser");
 
 var app = express();
 app.set("view engine", "ejs"); // for EJS
-app.use(express.static('public')) // it tells to express o serve all the static file present in public folder.
+app.use(express.static("public")); // it tells to express o serve all the static file present in public folder.
 app.use(express.urlencoded({ extended: true }));
 
-var items = [];
-
-app.get("/", function (req, res) {
-  res.render("list", { ejes: items }); //render is used rather than send while using ejs
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todo");
+const trySchema = new mongoose.Schema({
+  name: String,
 });
 
-app.post("/", function (req, res) {
-  var item = req.body.ele1;
-  items.push(item);
-  res.redirect("/");
+const item = mongoose.model("task", trySchema);
+
+const todo = new item({
+  name: "todo app",
+});
+
+todo.save();
+
+app.get("/", function (req, res) {
+  item.find({}, function (err, items) {
+    if (!err) {
+      res.render("list", { ejes: items });
+    }
+  });
 });
 
 app.listen(8000, function () {
